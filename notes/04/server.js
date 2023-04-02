ce89aafe054cc2d21e83be74c2590ab92db0a97b
duclm278 <palered548@gmail.com>
2023-04-02 22:10:18 +0700

    Swap files

--- notes/04/server.js
+++ notes/04/server.js
@@ -1,10 +1,47 @@
-// NPM
+// Requests & Responses
 const http = require("http");
 const fs = require("fs");
-const _ = require("lodash");
 
 const server = http.createServer((req, res) => {
+  // 1. Request object
+  // console.log(req);
   console.log(req.url, req.method);
+
+  // 2. Response object
+  // res.setHeader("Content-Type", "text/plain");
+  // res.write("hello, ninjas");
+  // res.end();
+
+  // 2.1. Tags like <html>, <head>, <body> are added automatically by browser.
+  // res.setHeader("Content-Type", "text/html");
+  // res.write("<h1>hello, ninjas</h1>");
+  // res.write("<p>hello again, ninjas</p>");
+  // res.end();
+
+  // 2.2. Replace default <head> tag added by browser
+  // res.setHeader("Content-Type", "text/html");
+  // res.write("<head><link rel='stylesheet' href='#'></head>");
+  // res.write("<h1>hello, ninjas</h1>");
+  // res.write("<p>hello again, ninjas</p>");
+  // res.end();
+
+  // 3. Return html pages
+  // res.setHeader("Content-Type", "text/html");
+  // fs.readFile("./views/index.html", (err, data) => {
+  //   if (err) {
+  //     console.log(err);
+  //     res.end();
+  //   } else {
+  //     // `res.write` can be used multiple times to write multiple chunks of data.
+  //     // res.write(data);
+  //     // res.end();
+  //     // Optional `chunk` and `encoding` arguments allow one final additional
+  //     // chunk of data to be written immediately before closing stream.
+  //     res.end(data);
+  //   }
+  // });
+
+  // 4. Basic routing
   res.setHeader("Content-Type", "text/html");
   let path = "./views/";
   switch (req.url) {
